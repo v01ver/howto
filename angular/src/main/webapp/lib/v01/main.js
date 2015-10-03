73dda7d70eb749fff903054b03c83ad62831fbaa
v01ver <aapala@gmail.com>
2015-10-03 23:33:38 +0300

    Order change

--- angular/src/main/webapp/lib/v01/main.js
+++ angular/src/main/webapp/lib/v01/main.js
@@ -4,28 +4,6 @@ var v01app = angular.module('v01app',  ['ngMessages'])
 	$rootScope.showhowto={};
 });
 
-
-var v01list = v01app.controller('listCtrl', function ($scope, $http, $rootScope, $compile, $element,$filter ) {
-	// List definitions, values and result in different data models
-	$scope.list = [ 
-	{name:'power.small', func:'Math.pow(x, 0.1234)'},
-	{name:'power.medium', func:'Math.pow(x, 1.234)'},
-	{name:'power.large', func:'Math.pow(x, 12.34)'}
-	];
-	$scope.values={};
-	$scope.result={};
-	
-	$scope.data = [
-	{name:'power.small', func:'Math.pow(x, 0.1234)', value: '',  result:'' },
-	{name:'power.medium', func:'Math.pow(x, 1.234)', value: '',  result:''},
-	{name:'power.large', func:'Math.pow(x, 12.34)', value: '',  result:''}
-	];
-	
-
-});
-
-
-
 var clauseresolver = v01app. directive('clauseresolver', function () {
     return {
         restrict: 'E, A, C',
@@ -53,11 +31,14 @@ var clauseresolverbyindex = v01app.directive('clauseresolverbyindex', function (
         link: function (scope, element, attrs) {
         	var self = this;
 			scope.$watch( attrs.watch , function(nw,ow){
+				console.log(nw);
+				if(nw){
 				if(nw[attrs.clausex] && nw[attrs.clause] && attrs.clauseresult){
 					var x = nw[attrs.clausex];
 					var clause = nw[attrs.clause];
 					var r = eval(clause);
 					nw[attrs.clauseresult] = r;
+				}
 				}
 			},true);
 			
@@ -65,6 +46,27 @@ var clauseresolverbyindex = v01app.directive('clauseresolverbyindex', function (
         scope: true
     };
 });
+
+
+var v01list = v01app.controller('listCtrl', function ($scope, $http, $rootScope, $compile, $element,$filter ) {
+	// List definitions, values and result in different data models
+	$scope.list = [ 
+	{name:'power.small', func:'Math.pow(x, 0.1234)'},
+	{name:'power.medium', func:'Math.pow(x, 1.234)'},
+	{name:'power.large', func:'Math.pow(x, 12.34)'}
+	];
+	$scope.values={};
+	$scope.result={};
+	
+	$scope.data = [
+	{name:'power.small', func:'Math.pow(x, 0.1234)', value: '',  result:'' },
+	{name:'power.medium', func:'Math.pow(x, 1.234)', value: '',  result:''},
+	{name:'power.large', func:'Math.pow(x, 12.34)', value: '',  result:''}
+	];
+	
+
+});
+
 
 var v01customvalidCtrl = v01app.controller('customvalidctrl', function ($scope, $http, $rootScope, $compile, $element,$filter ) {
 	$scope.list=[ 
